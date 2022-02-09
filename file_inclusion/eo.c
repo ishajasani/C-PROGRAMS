ad953c87bc5ddb8352ad657f361acc8d5c53a285
ishajasani <ishajasani2203@gmail.com>
2022-02-09 20:28:34 +0530

    File added

---
+++ file_inclusion/eo.c
@@ -0,0 +1,10 @@
+extern n;
+void call(){
+    if(n%2==0)
+    {
+        printf("Even");
+    }
+    else
+        printf("Odd");
+}
+
---
+++ file_inclusion/t3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "eo.c"
+int n;
+void main()
+{
+    printf("Enter number: ");
+    scanf("%d",&n);
+    call();
+}
